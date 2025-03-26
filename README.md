9d6585016cef90c2e91ea5739327efacfa4306ed
Luci-Nguyen <hoan98hanam@gmail.com>
2025-03-26 23:40:11 +0700

    "commit"

---
+++ README.md
@@ -0,0 +1,5 @@
+npm init -y
+npm install -g nodemon
+npm install express mysql2 body-parser cors dotenv
+npm install bcryptjs
+
--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const app = require("./src/app");
+const app = require("./app.js");
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
